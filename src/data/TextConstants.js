6df800bba12b9c18f1b05e203e5a7da13f649107
Hassaan Tahir <89857042+hassaantahir7@users.noreply.github.com>
2024-11-10 13:30:38 +0500

    Add files via upload

---
+++ src/data/TextConstants.js
@@ -0,0 +1,33 @@
+
+export const PAGE_TEXTS = {
+    SIGNUP: {
+        TITLE: 'Most Interesting and Effective Online Quran Learning.',
+        TEXT: 'AlQuranClasses is the best platform for online Quran learning. We have unique ways of teaching that empower kids, adults, women, men and new Muslims to learn the Quran Online from the comfort of your home.'
+    },
+    ADD_NEW_STUDENT: {
+        STUDENT_INFO: 'Student Information',
+        COURSE_INFO: 'Course Information',
+        AVAILABILITY:  'Student Availability',
+        TRIAL: 'Trial Information',
+    },
+};
+
+// customer sidebar links
+export const TopMenu = [
+    {
+        title: 'Home',
+        url: '/'
+    },
+    {
+        title: 'Products',
+        url: '/products'
+    },
+    {
+        title: 'About Us',
+        url: '/about'
+    },
+    {
+        title: 'Contact Us',
+        url: '/contact'
+    }
+];
